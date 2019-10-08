51b53c437398c22fdd4b384d9d8419845eba98cf
Sloth Thy Lord <Sloth Thy Lord@TocinoSublime>
2019-10-08 17:08:41 +0200

    Filemanager fully working (must test)

--- src/fileManager/FileManager.java
+++ src/fileManager/FileManager.java
@@ -1,14 +1,15 @@
 package fileManager;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class FileManager {
 	public FileManager(){
@@ -19,17 +20,20 @@ public class FileManager {
 		if(!conf.isFile()) {
 			try {
 				conf.createNewFile();
+				conf = new File(path+"\\reports");
+				conf.mkdir();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
-				System.out.println("not possible to create config File");
+				System.out.println("not possible to create config File or folder...");
 			}
 		}
 	}
+	
 	public void writeUserConfig(String name, String surname, int age, int weight,char gender) {
 		File manager= new File(System.getProperty("user.dir")+"\\config log.txt");
+		PrintWriter data = null;
 		try {
-			PrintWriter data = new PrintWriter(manager);
+			data = new PrintWriter(manager);
 			data.println(name);
 			data.println(surname);
 			data.println(age);
@@ -37,8 +41,15 @@ public class FileManager {
 			data.println(gender);
 			data.close();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			System.out.println("could not write user config data");
 			e.printStackTrace();
+		}finally {
+			try {
+				data.close();
+			}catch(Exception e) {
+				System.out.println("could not close properly the file");
+				e.printStackTrace();
+			}
 		}
 	}
 	public boolean isConfigured() {
@@ -51,8 +62,9 @@ public class FileManager {
 	public String readUserConfig() {
 		File manager= new File(System.getProperty("user.dir")+"\\config log.txt");
 		String record;
+		BufferedReader data=null;
 		try {
-			BufferedReader data= new BufferedReader(new InputStreamReader(new FileInputStream(manager)));
+			data= new BufferedReader(new InputStreamReader(new FileInputStream(manager)));
 			record="";
 			String readLine="";
 			
@@ -61,10 +73,115 @@ public class FileManager {
 			}
 			
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			record="";
+		}finally {
+			try {
+				data.close();
+			}catch(Exception e) {
+				System.out.println("could not read user config");
+				e.printStackTrace();
+			}
 		}
 		return record;
+	}
+	public void writeData(List<Double>[]ecg, List<Double>[]eeg) {
+		int counter=1;
+		File manager = new File(System.getProperty("user.dir")+"\\reports\\report_"+counter);
+		while(manager.isFile()) {
+			counter++;
+		}
+		manager= new File(System.getProperty("user.dir")+"\\reports\\report_"+counter);
+		PrintWriter data =null;
+		try {
+			manager.createNewFile();
+			data= new PrintWriter(manager);
+			Iterator iterator1= eeg[1].iterator();
+			Iterator iterator3= ecg[1].iterator();
+			
+			data.println("EEG DATA");
+			for (Iterator iterator = eeg[0].iterator(); iterator.hasNext();) {
+				data.print(iterator.next());
+				data.print(" ");
+				data.print(iterator1.next());
+				data.println();
+			}
+			data.println("ECG DATA");
+			for (Iterator iterator2 = ecg[0].iterator(); iterator2.hasNext();) {
+				data.print(iterator2.next());
+				data.print(" ");
+				data.print(iterator3.next());
+				data.println();
+			}
+		}catch(Exception e) {
+			System.out.println("could not create report file");
+			e.printStackTrace();
+		}finally {
+			try {
+				data.close();
+			}catch(Exception e) {
+				System.out.println("could not close report file");
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	public List[] readData(String path) {
+		ArrayList<Double> time1 = new ArrayList <Double>();
+		ArrayList<Double> time2 = new ArrayList <Double>();
+		ArrayList<Double> ecg = new ArrayList <Double>();
+		ArrayList<Double> eeg = new ArrayList <Double>();
+		
+		File manager = new File(path);
+		BufferedReader data= null;
+		try {
+			data = new BufferedReader(new InputStreamReader(new FileInputStream(manager)));
+			String inputRead;
+			boolean phaseOneComplete=false;
+			System.out.println(data.readLine());
+			while((inputRead=data.readLine())!=null) {
+				
+				char[] temp=inputRead.toCharArray();
+				double time=0;
+				double input=0;
+				
+				if(inputRead.equals("ECG DATA")) {
+					phaseOneComplete=true;
+					continue;
+				}else {
+					String helper="";
+					for (int i = 0; i < temp.length; i++) {
+						if(temp[i]!=' ') {
+							helper=helper+temp[i];
+						}else {
+							time=Double.parseDouble(helper);
+							helper="";
+						}
+						if(i==temp.length-1) {
+							input=Double.parseDouble(helper);
+						}
+					}
+				}
+				if(phaseOneComplete) {
+					time2.add(time);
+					ecg.add(input);
+				}else {
+					time1.add(time);
+					eeg.add(input);
+				}
+			}
+			try {
+				data.close();
+			}catch(Exception e) {
+				System.out.println("could not close reader");
+				e.printStackTrace();
+			}
+			return new List[] {time1,ecg,time2,eeg};
+		} catch (Exception e) {
+			System.out.println("could not read report");
+			e.printStackTrace();
+			return null;
+		}
+		
 	}
 }
